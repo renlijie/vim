667c93748ba7f05a7783d120a8c8135321f7ab96
Lijie Ren <lijie.cs@gmail.com>
2013-07-25 15:41:03 -0700

    improve setup.sh to clean_restore.sh

---
+++ clean_restore.sh
@@ -0,0 +1,24 @@
+echo ">>> brew install macvim"
+brew install macvim
+echo ""
+
+echo ">>> git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle"
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+echo ""
+
+echo ">>> creating .vimrc and .gvimrc"
+echo "source $HOME/.vim/_vimrc" > ~/.vimrc
+echo "source $HOME/.vim/_gvimrc" > ~/.gvimrc
+echo ""
+
+echo ">>> BundleInstall"
+mvim +BundleInstall +qall
+
+echo ">>> compiling YouCompleteMe"
+cd ~/.vim/bundle/YouCompleteMe
+./install.sh --clang-completer
+echo ""
+
+echo "done"
+vi ~/.vim/_vimrc
+
--- setup.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install vim
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-echo "source $HOME/.vim/_vimrc" > ~/.vimrc
-vim +BundleInstall +qall
-vim ~/.vimrc
