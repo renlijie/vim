f118ee7ce228de9c45c5cfb89c6342ce309ac943
Lijie Ren <lijie.cs@gmail.com>
2013-07-25 21:55:03 -0700

    copy rc files the last

--- clean_restore.sh
+++ clean_restore.sh
@@ -6,17 +6,17 @@ echo ">>> git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle"
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 echo ""
 
-echo ">>> creating .vimrc and .gvimrc"
-echo "source $HOME/.vim/_vimrc" > ~/.vimrc
-echo "source $HOME/.vim/_gvimrc" > ~/.gvimrc
-echo ""
-
 echo ">>> BundleInstall"
 mvim +BundleInstall +qall
 
 echo ">>> compiling YouCompleteMe"
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh --clang-completer
+echo ""
+
+echo ">>> creating .vimrc and .gvimrc"
+echo "source $HOME/.vim/_vimrc" > ~/.vimrc
+echo "source $HOME/.vim/_gvimrc" > ~/.gvimrc
 echo ""
 
 echo "done"
