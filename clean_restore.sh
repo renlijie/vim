8d0cf050afda978c4432fc99efe33715dd67ce95
Lijie Ren <lijie.cs@gmail.com>
2013-08-29 14:27:40 -0700

    fix cd in clean_restore.sh

--- clean_restore.sh
+++ clean_restore.sh
@@ -1,24 +1,27 @@
-echo ">>> brew install macvim"
-brew install macvim
-echo ""
+function clean_install() {
+  echo ">>> brew install macvim"
+  brew install macvim
+  echo ""
 
-echo ">>> git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle"
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-echo ""
+  echo ">>> git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle"
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+  echo ""
 
-echo ">>> BundleInstall"
-mvim +BundleInstall +qall
+  echo ">>> BundleInstall"
+  mvim +BundleInstall +qall
 
-echo ">>> compiling YouCompleteMe"
-cd ~/.vim/bundle/YouCompleteMe
-./install.sh --clang-completer
-echo ""
+  echo ">>> compiling YouCompleteMe"
+  cd ~/.vim/bundle/YouCompleteMe
+  ./install.sh --clang-completer
+  echo ""
 
-echo ">>> creating .vimrc and .gvimrc"
-echo "source $HOME/.vim/_vimrc" > ~/.vimrc
-echo "source $HOME/.vim/_gvimrc" > ~/.gvimrc
-echo ""
+  echo ">>> creating .vimrc and .gvimrc"
+  echo "source $HOME/.vim/_vimrc" > ~/.vimrc
+  echo "source $HOME/.vim/_gvimrc" > ~/.gvimrc
+  echo ""
 
-echo "done"
-vi ~/.vim/_vimrc
+  echo "done"
+  vi ~/.vim/_vimrc
+}
 
+clean_install
